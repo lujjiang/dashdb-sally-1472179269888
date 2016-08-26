1cd661e4c474fdb96269e925e5d9a862bf901676
sally <lujjiang@cn.ibm.com>
2016-08-26 12:57:49 +0800

    error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 //we put all utilities in utils folder, for example, config, logging
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
