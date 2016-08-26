d643820dd0a4c72fede7106284fa86ad6aa19d8c
sally <lujjiang@cn.ibm.com>
2016-08-26 14:17:22 +0800

    app manage

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,39 @@ exports.listSysTables = function(ibmdb,connString) {
 	   
 	}
 }
+exports.insertTweets = function(ibmdb,connString) {
+    return function(req, res) {
+
+	   	   
+       ibmdb.open(connString, function(err, conn) {
+			if (err ) {
+			 res.send("error occurred " + err.message);
+			}
+			else {
+				conn.query("SELECT FIRST_NAME, LAST_NAME, EMAIL, WORK_PHONE from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY", function(err, tables, moreResultSets) {
+							
+							
+				if ( !err ) { 
+					res.json(tables);
+
+					
+				} else {
+				   res.send("error occurred " + err.message);
+				}
+
+				/*
+					Close the connection to the database
+					param 1: The callback function to execute on completion of close function.
+				*/
+				conn.close(function(){
+					console.log("Connection Closed");
+					});
+				});
+			}
+		} );
+	   
+	}
+}
 exports.count = function(req, res){
 	insightRequest("/count", req.param("q"), function(err, data) {
         if (err) {
